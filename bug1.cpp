d35ce738325c221aa428be127ca10686c8fde381
rodcaseribeiro <rodolfo2804@hotmail.com>
2017-12-07 14:01:39 -0200

    Create bug1.cpp

---
+++ bug1.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <string.h>
+int fn1(char *str){
+    char local[49];
+    strcpy(local,str);
+return 0;
+}
+int main(int argc,char * args[]){
+fn1(args[1]);
+return 0;
+}
